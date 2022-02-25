5e6cf22c90b919c04f884ade42776985de9fb0d7
daigoro <daigoropersson@gmail.com>
2022-02-26 00:08:22 +0100

    G. 1

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const mongoose = require('mongoose')
+const {User} = require('./models/user')
+
+const app = express()
+const PORT = 3000;
+
+app.use(express.urlencoded())
+
+
+app.post('/signup', async (req, res) => {
+  const users = new User(req.body)
+  try {
+    await users.save()
+    res.redirect('/login')
+  } catch(error) {
+  res.send(error)
+  }
+
+})
+
+app.get("/signup", (req, res) => {
+    res.render("signup.ejs")
+  });
+
+app.post('/login', async (req,res) => {
+  console.log('Hej')
+  const username = req.body.username
+  const password = req.body.password
+  const user = await User.findOne({username: username, password: password})
+  console.log(user)
+  res.end()
+})
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs")
+});
+  
+mongoose.connect('mongodb://localhost/backend1-uppgift')
+app.listen(PORT, () => {
+  console.log(`Started Express server on port ${PORT}`)
+});
